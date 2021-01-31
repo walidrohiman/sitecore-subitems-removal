383f61db356d93b6a612f080cceb7041780e2fa8
Walid Rohiman <rohiman.walid@gmail.com>
2021-01-31 10:05:07 +0400

    Initial commit

---
+++ SitecoreExtension.Utilities/Commands/DeleteSelectedItems.cs
@@ -0,0 +1,51 @@
+﻿using Sitecore.Shell.Framework.Commands;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Globalization;
+using Sitecore.Text;
+using Sitecore.Web.UI.Sheer;
+using System.Collections.Specialized;
+using Sitecore;
+
+namespace SitecoreExtension.Utilities.Commands
+{
+    public class DeleteSelectedItems : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            Assert.ArgumentNotNull((object)context, nameof(context));
+            if (context.Items.Length != 1)
+                return;
+            Item obj = context.Items[0];
+            Context.ClientPage.Start(this, "Run", new NameValueCollection()
+            {
+                ["id"] = obj.ID.ToString(),
+                ["language"] = obj.Language.ToString(),
+                ["version"] = obj.Version.ToString()
+            });
+        }
+
+        protected void Run(ClientPipelineArgs args)
+        {
+            Assert.ArgumentNotNull((object)args, nameof(args));
+            Item itemNotNull = Client.GetItemNotNull(args.Parameters["id"], Language.Parse(args.Parameters["language"]), Sitecore.Data.Version.Parse(args.Parameters["version"]));
+            if (args.IsPostBack)
+            {
+                if (!args.HasResult)
+                {
+                    string load = $"item:refreshchildren(id={args.Parameters["id"]},language={args.Parameters["language"]},version={args.Parameters["version"]})";
+                    Context.ClientPage.ClientResponse.Timer(load, 1);
+                }
+            }
+            else
+            {
+                UrlString urlString = new UrlString(UIUtil.GetUri("control:DeleteSelectedChildren"));
+                urlString.Append("id", args.Parameters["id"]);
+                urlString.Append("la", args.Parameters["language"]);
+                urlString.Append("vs", args.Parameters["version"]);
+                SheerResponse.ShowModalDialog(urlString.ToString(), "1200px", "700px", string.Empty, true);
+                args.WaitForPostBack();
+            }
+        }
+    }
+}
---
+++ SitecoreExtension.Utilities/Dialogs/SetDeleteSubItems.cs
@@ -0,0 +1,63 @@
+﻿using Sitecore;
+using Sitecore.Data.Items;
+using Sitecore.Diagnostics;
+using Sitecore.Shell.Applications.ContentEditor;
+using Sitecore.Web.UI.Pages;
+using Sitecore.Configuration;
+using System;
+using Sitecore.Data;
+using System.Collections.Generic;
+using Sitecore.SecurityModel;
+
+namespace SitecoreExtension.Utilities.Dialogs
+{
+    public class SetDeleteSubItems : DialogForm
+    {
+        protected TreeList treeList;
+
+        protected Database database = Factory.GetDatabase("master");
+        protected override void OnLoad(EventArgs e)
+        {
+            Assert.ArgumentNotNull(e, nameof(e));
+            base.OnLoad(e);
+            if (Context.ClientPage.IsEvent)
+                return;
+            Item itemFromQueryString = UIUtil.GetItemFromQueryString(Context.ContentDatabase);
+            Assert.IsNotNull(itemFromQueryString, "Item not found.");
+            treeList.SetValue(itemFromQueryString[FieldIDs.Branches]);
+            treeList.ItemLanguage = Context.Language.Name;
+
+            treeList.Source = itemFromQueryString.Paths.FullPath;
+
+            List<string> arrayItems = new List<string>();
+
+            foreach (Item child in itemFromQueryString.Children)
+            {
+                arrayItems.Add(child.Name);
+            }
+
+            treeList.ExcludeItemsForDisplay = itemFromQueryString.Name;
+            treeList.IncludeItemsForDisplay = string.Join(",", arrayItems);
+        }
+
+        protected override void OnOK(object sender, EventArgs args)
+        {
+            Assert.ArgumentNotNull(sender, nameof(sender));
+            Assert.ArgumentNotNull(args, nameof(args));
+
+            var arrayItem = treeList.GetValue().Split('|');
+
+            foreach (var item in arrayItem)
+            {
+                Item sample = database.GetItem(new ID(item));
+
+                using (new SecurityDisabler())
+                {
+                    sample.Recycle();
+                }
+            }
+
+            base.OnOK(sender, args);
+        }
+    }
+}
